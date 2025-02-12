8f8343a471d144ab41e54c3ed9aa1cdf728dac9b
CloudXavi <cloudynek@proton.me>
2025-02-12 12:19:17 +0100

    Update chat.php

--- lbs/chat.php
+++ lbs/chat.php
@@ -9,7 +9,7 @@
 </head>
 <body>
    
-<nav id="back" style="color: aliceblue;"> <a href="index.php"> << Powrót << </a> </nav>  <br />
+<nav id="back" style="color: aliceblue;"> <a href="index.html"> << Powrót << </a> </nav>  <br />
 
 <h1 style="font-size: 100px; text-align: center; margin-top:700px;"> Witaj na stronie! Na razie nic tu nie ma! </h1>
 
